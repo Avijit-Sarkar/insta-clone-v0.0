ee10f4155fb2e66ea0c3a71e4ceabb3ad7573283
Avijit <avijitsarkar2222@gmail.com>
2023-04-01 22:43:09 +0530

    added the header logo

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,30 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
   return (
     <div>
-      <h1>I am Header</h1>
-
-      {/* Left */}
-      {/* Middle */}
-      {/* Right */}
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+          <Image
+            src="http://links.papareact.com/ocw"
+            alt=""
+            objectFit="contain"
+            layout="fill"
+          />
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="http://links.papareact.com/jjm"
+            alt=""
+            objectFit="contain"
+            layout="fill"
+          />
+        </div>
+        {/* Middle - input search filed */}
+        {/* Right */}
+      </div>
     </div>
   );
 }
