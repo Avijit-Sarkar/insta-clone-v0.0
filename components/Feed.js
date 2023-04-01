f6bcd621268a45802fe59494b6772051afbc9cec
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 00:15:04 +0530

    structuring the feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <main>
+      {/* Section */}
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+
+      {/* secrion */}
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
+    </main>
+  );
 }
 
 export default Feed;
