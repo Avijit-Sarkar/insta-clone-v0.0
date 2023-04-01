1655224ba0043d3b8ba5be9d7d12dc357017a48d
Avijit <avijitsarkar2222@gmail.com>
2023-04-01 23:22:09 +0530

    middle input search field done.

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ function Header() {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid  w-24 cursor-pointer">
           <Image
             src="http://links.papareact.com/ocw"
             alt=""
@@ -22,7 +22,7 @@ function Header() {
             layout="fill"
           />
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="http://links.papareact.com/jjm"
             alt=""
@@ -31,11 +31,17 @@ function Header() {
           />
         </div>
         {/* Middle - input search filed */}
-        <div>
-          <div>
-            <SearchIcon className="h-5 w-5" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input type="text" placeholder="Search" />
         </div>
         {/* Right */}
       </div>
