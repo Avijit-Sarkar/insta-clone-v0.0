388827c9c9df8f9fca8b23d11c656ebf0a2ebd9b
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 00:31:40 +0530

    creating stories and css

--- components/Feed.js
+++ components/Feed.js
@@ -3,7 +3,7 @@ import Stories from "./Stories";
 
 function Feed() {
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3">
       {/* Section */}
       <section>
         {/* Stories */}
