15626b2b7201e198ff682ecd3582eab0774365c9
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 21:55:04 +0530

    Adding a dialog transition

--- components/Modal.js
+++ components/Modal.js
@@ -1,12 +1,20 @@
 import { modalState } from "@/atoms/modalAtoms";
-import { Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import React, { Fragment } from "react";
 import { useRecoilState } from "recoil";
 
 function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
 
-  return <Transition.Root show={open} as={Fragment}></Transition.Root>;
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-hidden"
+        onClose={setOpen}
+      ></Dialog>
+    </Transition.Root>
+  );
 }
 
 export default Modal;
