e7176cc34ea31e26d6077de3f730d76f24bf338c
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 15:06:50 +0530

    Added dummy Data for the post

--- components/Posts.js
+++ components/Posts.js
@@ -1,6 +1,25 @@
 import React from "react";
 import Post from "./Post";
 
+const posts = [
+  {
+    id: "123",
+    username: "DinoYt",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption:
+      "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
+  },
+  {
+    id: "123",
+    username: "DinoYt",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption:
+      "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
+  },
+];
+
 function Posts() {
   return (
     <div>
