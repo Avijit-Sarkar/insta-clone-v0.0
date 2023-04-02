45fcf7a782ff30f074da928ed6d9ff13a31cb21d
Avijit <avijitsarkar2222@gmail.com>
2023-04-03 01:32:58 +0530

    getting the post in the frontend

--- components/Posts.js
+++ components/Posts.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
 
-const posts = [
-  {
-    id: "123",
-    username: "DinoYt",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption:
-      "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
-  },
-  {
-    id: "123",
-    username: "DinoYt",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption:
-      "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
-  },
-];
+// const posts = [
+//   {
+//     id: "123",
+//     username: "DinoYt",
+//     userImg: "https://links.papareact.com/3ke",
+//     img: "https://links.papareact.com/3ke",
+//     caption:
+//       "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
+//   },
+//   {
+//     id: "123",
+//     username: "DinoYt",
+//     userImg: "https://links.papareact.com/3ke",
+//     img: "https://links.papareact.com/3ke",
+//     caption:
+//       "Dino Yt is the first person in the world of the Internet and the first person in the Internet",
+//   },
+// ];
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => setPosts(snapshot.docs)
+      ),
+    [db]
+  );
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
