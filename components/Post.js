81599f5464c8ba1f2bb3e2c7881988e8846363a4
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 15:13:11 +0530

    structuring the post items and receving the props

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function Post() {
+function Post({ id, username, userImg, img, caption }) {
   return (
     <div>
       <h1>I am a post</h1>
+
+      {/* Header */}
+
+      {/* img */}
+
+      {/* Buttons */}
+
+      {/* caption */}
+
+      {/* comments */}
+
+      {/* input box */}
     </div>
   );
 }
