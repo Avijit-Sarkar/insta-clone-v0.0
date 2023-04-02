409f270e33c55b6dae539cd213d5ec42528aa0da
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 21:15:57 +0530

    updated the header

--- components/Header.js
+++ components/Header.js
@@ -11,15 +11,19 @@ import {
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
   const { data: session } = useSession();
-  console.log(session);
+  const router = useRouter();
   return (
     <div className="shadow-md border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid  w-24 cursor-pointer">
+        <div
+          onClick={() => router.push("/")}
+          className="relative hidden lg:inline-grid  w-24 cursor-pointer"
+        >
           <Image
             src="http://links.papareact.com/ocw"
             alt=""
@@ -27,7 +31,10 @@ function Header() {
             layout="fill"
           />
         </div>
-        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div
+          onClick={() => router.push("/")}
+          className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer"
+        >
           <Image
             src="http://links.papareact.com/jjm"
             alt=""
@@ -36,7 +43,7 @@ function Header() {
           />
         </div>
         {/* Middle - input search filed */}
-        <div className="max-w-xs">
+        <div className={`max-w-xs ${!session && "opacity-0"}`}>
           <div className="relative mt-1 p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
@@ -50,7 +57,7 @@ function Header() {
         </div>
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
-          <HomeIcon className="navBtn" />
+          <HomeIcon onClick={() => router.push("/")} className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
 
           {session ? (
