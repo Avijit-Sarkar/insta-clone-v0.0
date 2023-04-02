4850df00616a2c4c06a1c778ded9e45a31ebb185
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 17:19:23 +0530

    suggestions section done

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -29,6 +29,16 @@ function Suggestions() {
             className="w-10 h-10 rounded-full border p-[2px]"
             alt=""
           />
+
+          <div className="flex-1 ml-4">
+            <h2 className="font-semibold text-sm">{profile.username}</h2>
+            <h3 className="text-sm text-gray-400">
+              Works at {profile.company.name}
+            </h3>
+          </div>
+          <button className="text-blue-400 text-xs font-semibold">
+            Follow
+          </button>
         </div>
       ))}
     </div>
