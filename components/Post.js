61fe621fe8220f30da0173d8b51acb5e47a541a1
Avijit <avijitsarkar2222@gmail.com>
2023-04-02 15:39:48 +0530

    Post header section done

--- components/Post.js
+++ components/Post.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import {
   BookmarkIcon,
@@ -12,10 +13,17 @@ import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
-      <h1>I am a post</h1>
-
+    <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt=""
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
 
       {/* img */}
 
