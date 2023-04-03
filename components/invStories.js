d4e28d7359350adae3c2b399639c6ec050c99f11
Avijit <avijitsarkar2222@gmail.com>
2023-04-03 06:22:52 +0530

    final commit

--- components/invStories.js
+++ components/invStories.js
@@ -1,32 +1,79 @@
 const stories = [
   {
-    id: "123",
-    username: "DinoYt",
-    img: "https://picsum.photos/200",
-  },
-  {
-    id: "153",
+    id: "00153",
     username: "DinoYt",
     img: "https://links.papareact.com/3ke",
   },
 
   {
-    id: "456",
+    id: "00456",
     username: "Elon Musk",
     img: "https://links.papareact.com/kxk",
   },
   {
-    id: "396",
+    id: "00396",
     username: "Jeff Bezoz",
     img: "https://links.papareact.com/zvy",
   },
   {
-    id: "159",
+    id: "00159",
     username: "Mark Zuckerberg",
     img: "https://links.papareact.com/snf",
   },
   {
-    id: "963",
+    id: "00963",
+    username: "Bill Gates",
+    img: "https://links.papareact.com/zvy",
+  },
+  {
+    id: "01153",
+    username: "DinoYt",
+    img: "https://links.papareact.com/3ke",
+  },
+
+  {
+    id: "01456",
+    username: "Elon Musk",
+    img: "https://links.papareact.com/kxk",
+  },
+  {
+    id: "01396",
+    username: "Jeff Bezoz",
+    img: "https://links.papareact.com/zvy",
+  },
+  {
+    id: "01159",
+    username: "Mark Zuckerberg",
+    img: "https://links.papareact.com/snf",
+  },
+  {
+    id: "01963",
+    username: "Bill Gates",
+    img: "https://links.papareact.com/zvy",
+  },
+  {
+    id: "02153",
+    username: "DinoYt",
+    img: "https://links.papareact.com/3ke",
+  },
+
+  {
+    id: "02456",
+    username: "Elon Musk",
+    img: "https://links.papareact.com/kxk",
+  },
+  {
+    id: "02396",
+    username: "Jeff Bezoz",
+    img: "https://links.papareact.com/zvy",
+  },
+  {
+    id: "02159",
+    username: "Mark Zuckerberg",
+    img: "https://links.papareact.com/snf",
+  },
+  {
+    id: "02963",
     username: "Bill Gates",
     img: "https://links.papareact.com/zvy",
   },
